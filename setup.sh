f9319b7bcd9ec4ace693ff044b33ae6a06987c91
Joshi <ritesh.joshi@iiflw.com>
2020-02-07 02:12:40 +0530

    fixed config

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ ENV_FILE=${repo}/.env
 echo "Using docker_env file: $ENV_FILE"
 mkdir -p /var/log/cloudm/
 mkdir -p /var/media/cloudm/
-sudo cp ${repo}/docker/nginx_config /etc/nginx/sites-enabled/
+sudo cp ${repo}/nginx_config /etc/nginx/sites-enabled/
 sudo service nginx restart
 docker-compose -f ${repo}/docker-compose.yml up -d
